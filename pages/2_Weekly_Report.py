94c0504fa2b9abdefeef501cf62da330b18bbf3e
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2025-12-11 15:40:00 +0700

    Update 2_Weekly_Report.py

--- pages/2_Weekly_Report.py
+++ pages/2_Weekly_Report.py
@@ -25,18 +25,14 @@ df = load_data(st.secrets["matchdata"])
 #df2 = load_data(st.secrets["timeline"])
 
 col1, col2 = st.columns(2)
-
 with col1:
     gw = st.selectbox('Select GW', pd.unique(df['Gameweek']), key='2')
     temp = df[df['Gameweek']<=gw].reset_index(drop=True)
 with col2:
-    teamz = st.multiselect('Select Teams', pd.unique(df['Team']), key='1')
+    teamz = st.multiselect('Select Teams', pd.unique(temp['Team']), key='1')
     all_tms = st.checkbox('Select All Teams', key='3')
-    if all_tms:
-      datas = temp.copy()
-      teamz = pd.unique(datas['Team'])
-    else:
-      datas = temp[temp['Team'].isin(teamz)].reset_index(drop=True)
+if all_tms:
+    teamz = temp['Team'].unique().tolist()
 
 stands = standings(temp)
 s_chart = standings_chart(temp)
