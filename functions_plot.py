41bc2fa8aec45369bfc2635f36553cfd32af91fd
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2025-12-11 14:23:50 +0700

    Create functions_plot.py

---
+++ functions_plot.py
@@ -0,0 +1,70 @@
+def create_chart(teamz, data):
+  datas = data.copy()
+  teams = datas['Team'].tolist()
+  teams = reversed(teams)
+  match_day = datas.columns.tolist()[1:]
+  season_dict = datas.set_index('Team').T.to_dict('list')
+  team_list = teamz
+  highlight_dict = {
+      "Arema FC": "#0088CC",
+      "PERSIB": "#00599C",
+      "PSIM Yogyakarta": "#003366",
+      "Semen Padang FC": "#D40027",
+      "PERSIS": "#A51C30",
+      "PSM Makassar": "#E31D1A",
+      "PERSIJAP": "#CC0000",
+      "Bali United FC": "#E31D24",
+      "Madura United FC": "#D9232D",
+      "Malut United FC": "#D91E18",
+      "PERSIJA": "#E71D3A",
+      "Bhayangkara Presisi Lampung FC": "#FFD700",
+      "Borneo FC Samarinda": "#8F1C21",
+      "Dewa United Banten FC": "#000000",
+      "PERSEBAYA Surabaya": "#008B4E",
+      "PERSIK Kediri": "#591F79",
+      "PERSITA": "#4E2A84",
+      "PSBS Biak": "#87CEEB"
+  }
+  selected_teams = {
+      key: highlight_dict[key]
+      for key in team_list
+      if key in highlight_dict
+      }
+
+  bumpy = Bumpy(
+      background_color="#FFFFFF", scatter_color="#8FA1D2",
+      label_color="#1F4196", line_color="#BACAF7",
+      ticklabel_size=17, label_size=30,
+      scatter_primary='D',
+      show_right=False,
+      plot_labels=True,
+      alignment_yvalue=0.5,
+      alignment_xvalue=0.065
+  )
+
+  fig, ax = bumpy.plot(
+      x_list=match_day,
+      y_list=np.linspace(1, 18, 18).astype(int),
+      values=season_dict,
+      secondary_alpha=0.5,
+      highlight_dict=selected_teams,
+      figsize=(34, 18),
+      ylim=(-0.1, 20),
+      lw=2.5,
+      fontproperties=bold,
+  )
+
+  DC_to_FC = ax.transData.transform
+  FC_to_NFC = fig.transFigure.inverted().transform
+  DC_to_NFC = lambda x: FC_to_NFC(DC_to_FC(x))
+
+  for index, team in enumerate(teams):
+      ax_coords = DC_to_NFC([13.25, index+0.5])
+      logo_ax = fig.add_axes([ax_coords[0], ax_coords[1], 0.035, 0.035], anchor = "C")
+      club_icon = Image.open('./logo/'+team+'.png')
+      logo_ax.imshow(club_icon)
+      logo_ax.axis("off")
+  
+  fig.savefig('chart.jpg', dpi=500, bbox_inches='tight', facecolor=fig.get_facecolor(), edgecolor='none')
+  
+  return fig
